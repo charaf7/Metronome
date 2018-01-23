2ad02ee6223a6b437d611cbc4c8a865060c71d5d
Nicosvge <nicolas.sauvagevllaroy@gmail.com>
2018-01-23 15:27:25 +0100

    edit sprite anecdote

--- javaScript/chatelet.js
+++ javaScript/chatelet.js
@@ -12,5 +12,90 @@ $(document).ready(function()
     }
     );
     
+      $( "#drakkar-hover" ).hover(
+        function() {
+      
+            $( "#drakkar-hover" ).css('display', 'block');
+             $( "#drakkar" ).css('display', 'none');
+            
+        }, function() {
+      
+            $( "#drakkar-hover" ).css('display', 'none');
+             $( "#drakkar" ).css('display', 'block');
+    }
+    );
+    
+    
+    $( "#barreau" ).hover(
+        function() {
+      
+            $( "#barreau-hover" ).css('display', 'block');
+        }, function() {
+      
+            $( "#barreau-hover" ).css('display', 'none');
+    }
+    );
+    
+      $( "#barreau-hover" ).hover(
+        function() {
+      
+            $( "#barreau-hover" ).css('display', 'block');
+             $( "#barreau" ).css('display', 'none');
+            
+        }, function() {
+      
+            $( "#barreau-hover" ).css('display', 'none');
+             $( "#barreau" ).css('display', 'block');
+    }
+    );
+    
+    $( "#cadavre" ).hover(
+        function() {
+      
+            $( "#cadavre-hover" ).css('display', 'block');
+        }, function() {
+      
+            $( "#cadavre-hover" ).css('display', 'none');
+    }
+    );
+    
+      $( "#cadavre-hover" ).hover(
+        function() {
+      
+            $( "#cadavre-hover" ).css('display', 'block');
+             $( "#cadavre" ).css('display', 'none');
+            
+        }, function() {
+      
+            $( "#cadavre-hover" ).css('display', 'none');
+             $( "#cadavre" ).css('display', 'block');
+    }
+    );
+    
+     $( "#prostitue" ).hover(
+        function() {
+      
+            $( "#prostitue-hover" ).css('display', 'block');
+        }, function() {
+      
+            $( "#prostitue-hover" ).css('display', 'none');
+    }
+    );
+    
+      $( "#prostitue-hover" ).hover(
+        function() {
+      
+            $( "#prostitue-hover" ).css('display', 'block');
+             $( "#prostitue" ).css('display', 'none');
+            
+        }, function() {
+      
+            $( "#prostitue-hover" ).css('display', 'none');
+             $( "#prostitue" ).css('display', 'block');
+    }
+    );
+    
+    
+    
 });
       
