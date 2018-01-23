ba40dc0347204c02e2e0dfa0007a64395b9ec778
Nicosvge <nicolas.sauvagevllaroy@gmail.com>
2018-01-23 11:17:25 +0100

    station 1 edit

---
+++ javaScript/chatelet.js
@@ -0,0 +1,16 @@
+$(document).ready(function()
+{
+ 
+    
+    $( "#drakkar" ).hover(
+        function() {
+      
+            $( "#drakkar-hover" ).css('display', 'block');
+        }, function() {
+      
+            $( "#drakkar-hover" ).css('display', 'none');
+    }
+    );
+    
+});
+      
